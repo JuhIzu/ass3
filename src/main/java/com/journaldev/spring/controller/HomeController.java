f7e7ca8ba656601938c39a1a05e7c1dc78c67888
Juliana Izu <izujuliana@gmail.com>
2021-05-18 18:00:31 +0100

    initial setup

---
+++ src/main/java/com/journaldev/spring/controller/HomeController.java
@@ -0,0 +1,96 @@
+package com.journaldev.spring.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import reactor.core.publisher.Mono;
+
+import java.sql.*;
+
+
+import com.journaldev.spring.model.User;
+
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public String user(@Validated User user, Model model) {
+		
+		WebClient client = WebClient.create("https://etlinuxapp.azurewebsites.net");
+
+
+		User response = client.get().uri("/user?id=" + user.getId())
+						.exchange()
+                               .block()
+                               .bodyToMono(User.class)
+                               .block();
+		model.addAttribute("userName", response.getName());
+		return "user";
+	}
+
+
+	// @CrossOrigin(origins = "*", allowedHeaders = "*")
+    // @RequestMapping("/user")
+    // public User user(@RequestParam(value = "id", required = false) String idString) {
+
+	// 	int uid = Integer.parseInt(idString);
+    //     String username = getUsername(uid);
+    //     return new User(uid, username);
+    // }
+
+	private static String getUsername(int id) {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:Users.db");
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+			String sql = "SELECT Name FROM Users WHERE Id = ?";			  
+			PreparedStatement pstmt  = c.prepareStatement(sql);
+
+            // set the value
+            pstmt.setInt(1,id);
+			
+            //
+            ResultSet rs  = pstmt.executeQuery();
+			
+			String name ="";
+            
+            // loop through the result set
+            while (rs.next()) {
+                name = rs.getString("Name");
+            }
+
+			return name;
+
+        }	
+		catch ( Exception e ) {
+			return e.toString(); //This would be a security issue in a production system
+		}
+   }
+}
